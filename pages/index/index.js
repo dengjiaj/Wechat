207ccdbccdaed15c2531adf7367c9ec8f87ba3c1
dengjiaj <465594616@qq.com>
2017-09-30 22:29:29 +0800

    修改样式
    修改底部导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,34 +2,34 @@
 //获取应用实例
 var app = getApp()
 Page({
-    data: {
-        userInfo: {},
-        item:23,
-        buttonArray:[{
-            name:"button_1",
-            id:"button_1",
-            text:"开始测试",
-            type:"primary",
-            even:"start",
-        }]
-    },
+  data: {
+    userInfo: {},
+    item:23,
+    buttonArray:[{
+      name:"button_1",
+      id:"button_1",
+      text:"开始测试",
+      type:"primary",
+      even:"start",
+    }]
+  },
+ 
 
-
-    //跳转到游戏界面
-    start:function(e){
-        wx.navigateTo({
-            url: '../test/test'
-        })
-    },
-    onLoad: function () {
-        console.log('onLoad')
-        var that = this
-        //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo){
-            //更新数据
-            that.setData({
-                userInfo:userInfo
-            })
-        })
-    }
+  //跳转到游戏界面
+  start:function(e){
+    wx.navigateTo({
+      url: '../test/test'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
 })
--- pages/queryresult/queryresult.js
+++ pages/queryresult/queryresult.js
@@ -2,79 +2,79 @@
 var app = getApp()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        addorsub: 0,
-        level: 0,
-        timetype: 0,
-    },
-    initNumber: function () {
-        var addorsub = this.data.addorsub;
-        var level = this.data.level;
-        var timetype = this.data.timetype;
-        console.log(addorsub)
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        this.data.userinfo = app.globalData.userinfo
-        this.setData({
-            userinfo: this.data.userinfo,
-            addorsub: app.addorsub,
-            level: app.level,
-            timetype: app.timetype,
-        })},
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    addorsub: 0,
+    level: 0,
+    timetype: 0,
+  },
+  initNumber: function () {
+    var addorsub = this.data.addorsub;
+    var level = this.data.level;
+    var timetype = this.data.timetype;
+    console.log(addorsub)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.data.userinfo = app.globalData.userinfo
+    this.setData({
+      userinfo: this.data.userinfo,
+      addorsub: app.addorsub,
+      level: app.level,
+      timetype: app.timetype,
+  })},
+  
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
 
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
 })
